86d591d3083f6ad41b3a224ba82a55d8ae7f6c0e
GrigAnd <andrey.grigorev03@gmail.com>
2020-09-15 11:09:01 +0300

    Updates

---
+++ precache-manifest.0bc2c1ce7be166eb2b87d1ed29996fd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "591a3a7e43426d15ea0f2e426ea4228d",
+    "url": "/VezdecodeVKMiniAppTaskiOS/index.html"
+  },
+  {
+    "revision": "08160c5820dd992db127",
+    "url": "/VezdecodeVKMiniAppTaskiOS/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "08160c5820dd992db127",
+    "url": "/VezdecodeVKMiniAppTaskiOS/static/js/2.8adaff6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VezdecodeVKMiniAppTaskiOS/static/js/2.8adaff6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2fdaafed9cc6f6c619d",
+    "url": "/VezdecodeVKMiniAppTaskiOS/static/js/main.e084359e.chunk.js"
+  },
+  {
+    "revision": "8593652ea68645772754",
+    "url": "/VezdecodeVKMiniAppTaskiOS/static/js/runtime-main.8dd49c6e.js"
+  }
+]);
